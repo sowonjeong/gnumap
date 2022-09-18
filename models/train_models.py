07d2c9bbc61b85a89186b998ec0ec8dae8d78cc5
donnate <cdonnat@uchicago.edu>
2022-09-18 14:35:11 -0700

    add carbon tracking

--- models/train_models.py
+++ models/train_models.py
@@ -97,6 +97,8 @@ def train_mvgrl(data, diff, out_dim, n_layers, patience=20,
     model = model.to(dev)
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=wd)
     loss_fn1 = nn.BCEWithLogitsLoss()
+    tracker = EmissionsTracker(project_name='MVGRL_'+ str(out_dim) + '_' +  name_file)
+    tracker.start()
     for epoch in range(epochs):
         model.train()
         optimizer.zero_grad()
@@ -126,7 +128,7 @@ def train_mvgrl(data, diff, out_dim, n_layers, patience=20,
         if cnt_wait == patience:
             print('Early stopping!')
             break
-
+    tracker.stop()
     print('Loading {}th epoch'.format(best_t))
     model.load_state_dict(torch.load(os.getcwd() + '/results/best_mvgrl_dim' +
                                      str(out_dim) + '_' + name_file +  '.pkl'))
@@ -184,6 +186,13 @@ def train_gnumap(data, dim, n_layers=2, target=None,
     #     edge_weights_neg = fast_intersection(row_neg[index_neg], col_neg[index_neg], edge_weights_neg,
     #                                          target, unknown_dist=1.0, far_dist=3.0)
     best_t=0
+    if target is None:
+        tracker = EmissionsTracker(project_name='GNUMAP_'+ str(dim) + '_'  method +
+                    + 'n_neighbours' + str(neighbours) + '_mindist_'+ str(min_dist) +  norm + '_' +  name_file)
+    else:
+        tracker = EmissionsTracker(project_name='semiGNUMAP_'+ str(dim) + '_'  method +
+                    + 'n_neighbours' + str(neighbours) + '_mindist_'+ str(min_dist) +  norm + '_' +  name_file)
+    tracker.start()
     for epoch in range(epochs):
         model.train()
         optimizer.zero_grad()
@@ -226,6 +235,7 @@ def train_gnumap(data, dim, n_layers=2, target=None,
         if cnt_wait == patience:
             print('Early stopping at epoch {}!'.format(epoch))
             break
+    tracker.stop()
     print('Loading {}th epoch'.format(best_t))
     model.load_state_dict(torch.load(os.getcwd() + '/results/best_gnumap_' +
                                      str(method) + '_neigh' + str(neighbours)
@@ -256,6 +266,9 @@ def train_grace(data, channels, proj_hid_dim, n_layers=2, tau=0.5,
         model = GRACE(in_dim, hid_dim, proj_hid_dim, n_layers, tau)
         model = model.to(dev)
         optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=wd)
+        tracker = EmissionsTracker(project_name='GRACE_'+ str(channels) +
+                    + '_proj_hid_dim' + str(proj_hid_dim) + '_tau'+ str(tau) +
+                    '_edr' + str(edr) + '_fmr'  +str(fmr) + '_' + proj + '_' +  name_file)
         def train_grace_one_epoch(model, data, fmr, edr, proj):
                 model.train()
                 optimizer.zero_grad()
@@ -268,10 +281,12 @@ def train_grace(data, channels, proj_hid_dim, n_layers=2, tau=0.5,
                 loss.backward()
                 optimizer.step()
                 return loss.item()
+        tracker.start()
         for epoch in range(epochs):
             loss = train_grace_one_epoch(model, data, fmr,
                                           edr, proj)
             print('Epoch={:03d}, loss={:.4f}'.format(epoch, loss))
+        tracker.stop()
         return(model)
 
 
@@ -303,6 +318,10 @@ def train_cca_ssg(data, channels, lambd=1e-5,
     model = CCA_SSG(in_dim, hid_dim, out_dim, n_layers, lambd, N, use_mlp=False) #
     model = model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=0)
+    tracker = EmissionsTracker(project_name='CCA-SSG_'+ str(channels) +
+                + '_lambda' + str(lambd) +
+                '_edr' + str(edr) + '_fmr'  +str(fmr) + '_' +  name_file)
+
     def train_cca_one_epoch(model, data):
         model.train()
         optimizer.zero_grad()
@@ -315,10 +334,11 @@ def train_cca_ssg(data, channels, lambd=1e-5,
         loss.backward()
         optimizer.step()
         return loss.item()
-
+    tracker.start()
     for epoch in range(epochs):
         loss = train_cca_one_epoch(model, data) #train_semi(model, data, num_per_class, pos_idx)
         # print('Epoch={:03d}, loss={:.4f}'.format(epoch, loss))
+    tracker.stop()
     return(model)
 
 
@@ -346,6 +366,12 @@ def train_bgrl(data, channels, lambd=1e-5,
     s = lambda epoch: epoch / 1000 if epoch < 1000 \
                     else ( 1 + np.cos((epoch-1000) * np.pi / (epochs - 1000))) * 0.5
     scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda = s)
+    tracker = EmissionsTracker(project_name='BGRL_'+ str(channels) +
+                + '_lambda' + str(lambd) +
+                '_edr' + str(edr) + '_fmr'  +str(fmr) +
+                '_drf1' + str(drf1) + '_dre1'  +str(dre1) + '_pred_hid' +
+                str(pred_hid) + '_' +  name_file)
+
 
     def train_bgrl_one_epoch(model, data):
         model.train()
