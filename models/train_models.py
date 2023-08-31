772584bbb376e97e882f647ae452654fa269878c
jiheeyy <86678801+jiheeyy@users.noreply.github.com>
2023-08-31 00:29:58 -0500

    delete logging path

--- models/train_models.py
+++ models/train_models.py
@@ -40,10 +40,8 @@ def init_weights(m):
 def train_dgi(data, hid_dim, out_dim, n_layers, dropout_rate=0.5, patience=20,
               epochs=200, lr=1e-3, name_file="1", device=None, gnn_type="symmetric", alpha=0.5, beta=1.0):
     current_path = os.getcwd()
-    logging.INFO(current_path)
     up_two_levels = os.path.dirname(os.path.dirname(current_path))
     directory_path = os.path.join(up_two_levels, "experiments/experiments/model_weights")
-    logging.INFO(directory_path)
 
     # if not os.path.exists(directory_path):
     #     os.makedirs(directory_path)
